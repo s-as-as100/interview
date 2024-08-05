d409bafdf4e9c163adf86cfd9779a8ff41c5eb7a
Mohd Arif <s.arifsaifi000@gmail.com>
2024-08-05 14:51:17 +0530

    max value inside the object

--- js1.js
+++ js1.js
@@ -457,10 +457,33 @@ console.log(typeof undefined)  // undefined --> type undefined
 
 // wa funtion to find the number of character in a string;
 
-function countCharacters(str) {
-    return str.length;
-}
+// function countCharacters(str) {
+//     return str.length;
+// }
 
-console.log(countCharacters("iloveyou"));
+// console.log(countCharacters("iloveyou"));
 
 // wa function to find the maximum value from an object
+
+
+function maxValueFromObject(obj) {
+    // convert into array
+    const values = Object.values(obj);
+    // console.log(array,"aa")
+    const maxValue = Math.max(...values);
+    return maxValue;
+    
+
+
+}
+
+
+let data = {
+    a:2,
+    b:5,
+    c:45,
+    d:1
+}
+
+console.log(maxValueFromObject(data)
+)
