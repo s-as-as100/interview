28a8f9b36279cd43fcdc805242bf777377709b34
Mohd Arif <s.arifsaifi000@gmail.com>
2024-08-09 23:08:12 +0530

    null vs undefined

--- basicJs.js
+++ basicJs.js
@@ -22,3 +22,57 @@
 
 // primitive values and objects
 
+// primitive values are boolens , number, string , null and undefined
+// All other values are objects.
+
+
+var obj1 = {}
+var obj2 = {}
+console.log(obj1===obj2) // false
+console.log(obj1===obj1) // true
+
+
+
+// undefind and null 
+
+
+// undefines means no values  -> uninitialzwd variables are undefined
+// --> Missing parameters are undefined
+// if you read a nonexistent property , you get undefined
+
+
+var x; 
+console.log(x)
+
+
+function sumRun(a,b) {
+  console.log(a,b)
+}
+
+sumRun(3) // point og execution // undefined 
+
+
+const ob ={
+
+}
+
+console.log(ob.name)  // undefined 
+
+
+
+// Null 
+
+// null means no object . it is used as a non value whenever an object is expected (parameters , last in chain of objects);
+
+
+function countLength(object) {
+    if(object===null) return -1
+   return  Object.keys(object).length
+    // if(object)   
+}
+
+console.log(countLength(null))
+
+
+
+
