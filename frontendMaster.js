c681de91ba9e01ffde0da4f6c167874d9bea46a4
Mohd Arif <s.arifsaifi000@gmail.com>
2024-10-14 19:44:19 +0530

    fix bugs solution

--- frontendMaster.js
+++ frontendMaster.js
@@ -1,20 +1,31 @@
-const str = "helloworld";
+// const str = "helloworld";
 
-const obj = {};
-for (const x of str) {
-    if(obj[x]){
-    obj[x]+=1;
-   }
-   else {
-    obj[x]=1
-   }
- }
+// const obj = {};
+// for (const x of str) {
+//     if(obj[x]){
+//     obj[x]+=1;
+//    }
+//    else {
+//     obj[x]=1
+//    }
+//  }
 
-for (let i = 0; i < str.length; i++) {
-   if (obj[str[i]]) {
-    obj[str[i]] += 1;
-  } else {
-    obj[str[i]] = 1;
-  }
+// for (let i = 0; i < str.length; i++) {
+//    if (obj[str[i]]) {
+//     obj[str[i]] += 1;
+//   } else {
+//     obj[str[i]] = 1;
+//   }
+// }
+// console.log(obj);
+
+function showMessage(marks) {
+  //  const msg = marks || 'Absent'; //bug   solve
+  const msg = marks ?? "Absent"; //bug   solve
+
+  console.log(`Marks ${msg}`);
 }
-console.log(obj);
+
+showMessage(12);
+showMessage(0);
+showMessage(435);
