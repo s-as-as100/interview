1541807a82a750b03cecf9db7c6caba8d04570ff
Mohd Arif <s.arifsaifi000@gmail.com>
2025-01-19 13:12:26 +0530

    d

--- basicDsaPractice.js
+++ basicDsaPractice.js
@@ -1,20 +1,17 @@
 // Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
-
 // Example 1:
-
 // Input: nums = [1, 2, 3, 3]
-
 // Output: true
 
 const nums = [1, 2, 3, 3];
 
 function isNumRepeated(nums) {
   let object = {};
-
   for (const val of nums) {
     if (object[val]) {
       return true;
     }
+    // {1:true}
     object[val] = true;
   }
   return false;
@@ -22,7 +19,9 @@ function isNumRepeated(nums) {
 
 console.log(isNumRepeated(nums));
 
-// const data = [1,2,3];
 
-// const result = data.includes(2);
-// console.log(result)
+// 2nd approach
+
+const data = nums.some((val) => val>1);
+console.log(data,"daaa")
+
